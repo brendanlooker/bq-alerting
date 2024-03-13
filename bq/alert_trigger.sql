90da845f5ea27b322bf379dc1b11586ea04525df
Brendan Buckley <46671710+brendanlooker@users.noreply.github.com>
2024-03-13 08:28:47 +0000

    Update alert_trigger.sql

--- bq/alert_trigger.sql
+++ bq/alert_trigger.sql
@@ -1,7 +1,7 @@
 -- Export data from the target table to GCS
 
 EXPORT DATA OPTIONS (
-  uri = 'gs://bq-alerting/bq-alerting*.csv',
+  uri = 'gs://bq-alerting/bq-alerting*.csv', # Update uri to relevant GCS bucket
   format = 'CSV',
   overwrite = true
 ) AS
